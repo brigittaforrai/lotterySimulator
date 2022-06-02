6c678ea15881e727e686398b5d47365d246eb57d
brigittaforrai <forraibrigi@gmail.com>
2022-06-02 08:40:49 +0200

    slider styles

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -2,7 +2,6 @@
     <div class="slider">
         <label for="slider">Speed</label>
         <input
-          ref="input"
           id="slider"
           v-model.number="value"
           type="range"
@@ -40,5 +39,43 @@ export default {
 </script>
 
 <style lang="scss">
-    
+    .slider {
+        width: 100%;
+        
+        input[type='range'] {
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            width: 100%;
+            height: 4px;
+            border-radius: 5px;  
+            background-color: $base-color;
+            outline: none;
+            -webkit-transition: .2s;
+            transition: opacity .2s;
+
+            &::-webkit-slider-thumb {
+                -webkit-appearance: none;
+                -moz-appearance: none;
+                appearance: none;
+                width: 14px;
+                height: 14px;
+                border-radius: 50%; 
+                background-color: white;
+                border: 1px solid $base-color;
+                cursor: pointer;
+
+                &:focus {
+                    background-color: red;
+                }
+            }
+
+            &::-moz-range-thumb {
+                width: 14px;
+                height: 14px;
+                border-radius: 50%;
+                background: $base-color;
+                cursor: pointer;
+            }
+        }
+    }
 </style>
